7c4de37d4b1ca462336142de59859a0fda0e2cf0
sepnic <sysu.zqlong@gmail.com>
2021-06-18 16:08:44 +0800

    Optimize function prototype for MD5Final()
    Signed-off-by: sepnic <sysu.zqlong@gmail.com>

--- include/cipher/md5.h
+++ include/cipher/md5.h
@@ -32,7 +32,7 @@ void MD5Init(struct MD5Context *ctx);
 
 void MD5Update(struct MD5Context *ctx, unsigned char const *buf, size_t len);
 
-void MD5Final(unsigned char digest[16], struct MD5Context *ctx);
+void MD5Final(struct MD5Context *ctx, unsigned char digest[16]);
 
 #ifdef __cplusplus
 }
--- source/cipher/md5.c
+++ source/cipher/md5.c
@@ -103,7 +103,7 @@ void MD5Update(struct MD5Context *ctx, unsigned char const *buf, size_t len)
  * Final wrapup - pad to 64-byte boundary with the bit pattern
  * 1 0* (64-bit count of bits processed, MSB-first)
  */
-void MD5Final(unsigned char digest[16], struct MD5Context *ctx)
+void MD5Final(struct MD5Context *ctx, unsigned char digest[16])
 {
   unsigned count;
   unsigned char *p;
